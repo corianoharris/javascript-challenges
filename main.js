14ca907cc1292d30f99d96866f88507d72e6798b
Coriano Harris <corianoharris@gmail.com>
2023-06-12 18:12:53 -0700

    Added Cornhole Challenge

--- main.js
+++ main.js
@@ -5,8 +5,6 @@
 const sentence =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-const result = document.querySelector("#longest-word-challenge-answer");
-
 let replacedWithSpaces = sentence.replace(/[^A-Za-z0-9]+/g, " ");
 let wordsArray = sentence.trim().split(" ");
 
@@ -21,8 +19,41 @@ function getThelongestWord() {
 }
 
 const getLongestWordAnswer = () => {
+  const result = document.querySelector("#longest-word-challenge-answer");
   const answer = getThelongestWord();
-  result.textContent = answer;
+  result.textContent = `Answer: ${answer}`;
+};
+
+// ===================================
+
+/**
+ * Cornhole Challenge
+ * valid points: 0, 1, 3
+ * example score: [0, 3, 3, 1]
+ */
+
+const cornholeBagsTossScore = [3, 3, 3, 3];
+
+const checkCornholeScore = () => {
+  let validTossPoint;
+  //sorting the length of words in the array from highest to lowest
+  for (let i = 0; i < cornholeBagsTossScore.length; i++) {
+    console.log(cornholeBagsTossScore[i]);
+    if (cornholeBagsTossScore[i] - 3) {
+      validTossPoint = true;
+    } else if (cornholeBagsTossScore[i] - 1) {
+      validTossPoint = true;
+    } else if (cornholeBagsTossScore[i] - 0) {
+      validTossPoint = true;
+    } else validTossPoint = false;
+  }
+  return validTossPoint;
+};
+
+const isCornholeScoreValid = () => {
+  const result = document.querySelector("#cornhole-challenge-answer");
+  const answer = checkCornholeScore();
+  result.textContent = `Answer: ${answer}`;
 };
 
 // ===================================
